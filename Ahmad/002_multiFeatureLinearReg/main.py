4dab02b7c591ef04a51c9eb3dc99006118f3b60d
Ahmad Hassan <ahmadhassanch@hotmail.com>
2018-01-18 18:29:09 +0500

    cleanup .

--- Ahmad/002_multiFeatureLinearReg/main.py
+++ Ahmad/002_multiFeatureLinearReg/main.py
@@ -4,32 +4,23 @@ import lossAndHypothesis
 import gradientDescent as gradDescent
 import sys
 sys.path.append('..')
-import utils.CSV.CSVutils as csv
+#import utils.CSV.CSVutils as csv
 
 
 def defineRanges():
-	# 2 feature example
-	#theta  = np.array([.93, 0.025,0.5]);
-	#noise  = np.array([0, 45,60]);
-	#xRange = np.array([[1,1],[200, 2000],[20, 250]]);
-
-	# 3 feature example
-	#theta  = np.array([-2, 4, 2, 3]);
-	#noise  = 1;
-	#xRange = np.array([[1,1],[-1, 3],[-5, 2],[0, 1]]);
-
+	noise = 1;
 	theta = [];
 	xRange = []
 	theta.append(-2); xRange.append([1, 1]);
 	theta.append(4); xRange.append([-1, 3]);
 	theta.append(2); xRange.append([-5, 2]);
 	theta.append(3); xRange.append([0, 1]);
-
-	return theta, xRange, noise
+	theta.append(-2.5); xRange.append([-2, 1]);
+	return np.array(theta), np.array(xRange), noise
 
 def makeData(m):
-	theta, xRange, noise= defineRanges()
-	X, y, yIdeal = generateData(theta, xRange, noise,  m)
+	thetaRef, xRange, noise = defineRanges()
+	X, y, yIdeal = generateData(thetaRef, xRange, noise,  m)
 	#csv.writeCSV(X, y, '03_multiFeatureLinearReg.csv')
 	#X, y = csv.readCSV('03_multiFeatureLinearReg.csv')
 	return X, y, yIdeal
@@ -37,6 +28,7 @@ def makeData(m):
 
 def main():
 	m = 1000;
+	
 
 	theta, xRange, noise =  defineRanges()	
 	N = theta.shape[0];                      # n = N -1  is the number of features
