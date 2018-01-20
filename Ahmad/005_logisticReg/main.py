dd2ab1419e474def1d95e7900f3987eb15a275d1
Ahmad Hassan <ahmadhassanch@hotmail.com>
2018-01-20 22:30:17 +0500

    Plots of theta and normalized theta OK

--- Ahmad/005_logisticReg/main.py
+++ Ahmad/005_logisticReg/main.py
@@ -4,104 +4,81 @@ import matplotlib.pyplot as plt
 sys.path.append('..')
 import utils.gradient.gradientDescent as gradDescent
 import utils.data.generateData as genData
-import utils.CSV.CSVutils as csv
 import utils.data.normalize as norm
-from mpl_toolkits.mplot3d.art3d import Poly3DCollection
-
-def sigmoid(z):
-	gx = 1/(1+np.exp(-z))
-	return gx
 
 def hypFunc(theta, x):
-	ht = x.dot(theta);
-	h = sigmoid(ht)
-	return h;
-
+	z = x.dot(theta);
+	gx = 1/(1+np.exp(-z))
+	return gx
 
 def lossFunc(theta, X, y):
 	global i;
 	m = X.shape[0];
 	h  = hypFunc(theta, X);
 	p1 = -y*np.log(h)
-	p2 = (1-y)* np.log(1.0-h)
+	p2 = (1-y)* np.log(1 - h)
 	J = np.mean(p1 - p2)
-	print i, theta, " == ", J
+	thetaNew = theta;
+	#thetaNew = thetaNew / thetaNew[0];
+	print i, thetaNew, " == ", J
 	i = i + 1;
 	return J;
-
 
 def defineThetaAndRanges():
 	theta = [];
 	xRange = []
-	theta.append(-2); xRange.append([1, 1]);
-	theta.append(3); xRange.append([-1, 1]);
-	theta.append(2); xRange.append([-1, 1]);
-	theta.append(5); xRange.append([-1, 1]);
+	theta.append(-2.0); xRange.append([1, 1]);
+	theta.append(3.0); xRange.append([-1, 1]);
+	theta.append(-2.0); xRange.append([-1, 1]);
+	theta.append(5.0); xRange.append([-1, 1]);
+
+	theta = theta / np.max(np.abs(theta));
 	return np.array(theta), np.array(xRange)
 
-
 def computeY(theta, X, noise):
-	#xt = X.dot(theta);
 	yt = hypFunc(theta, X)
-	#-X[:,1] + 0.5
+	#print X.shape
 	y = np.where(yt<0.5,0,1)
+
+	step = int(X.shape[0]* noise) 
+	
+	for i in range(step):
+		r = int(np.random.rand()*X.shape[0])
+		y[r] = 1 - y[r];
+
 	yIdeal = np.ones(y.shape)
 	return y, yIdeal
 
 
-i = 0;
-
-def plot3d(x1, x2, x3, y):
-	fig = plt.figure()
-	ax = fig.add_subplot(111,projection='3d')
-	ax.scatter(x1,x2,x3,c=y,marker='o')
-	ax.set_xlabel('X1 Feature X')
-	ax.set_ylabel('X2 Feature Y')
-	ax.set_zlabel('X3 Feature Z')
-	#plt.plot(x1,x2,h)
-
-#ax = Axes3D(fig)
-	x = [-1,1,1,-1]
-	y = [-1,-1,1,1]
-	z =[.5, .5, .5, .5]
-	verts = [zip(x, y,z)]
-	ax.add_collection3d(Poly3DCollection(verts))
-
-	plt.show()
 
 def main():
-	m = 100;
-	noise = 0.0;
+	m = 500;
+	noise = .02;  # enter percent noise
 	thetaRef, xRange = defineThetaAndRanges()
 	X, y, yIdeal = genData.generateData(thetaRef, xRange, noise, m, computeY)
-
-	plot3d(X[:,1],X[:,2], X[:,3],y)
-	exit()
+	#print X
+	#exit()
 	#X = norm.normalize(X)
 	N = X[0].shape[0]  # n = N -1  is the number of features
 
-	nIter = 7000;
-	alpha = 0.9
-	thetaEst = np.zeros(N);  # t0, t1, .... , tn+1
+	nIter = 1000;
+	alpha = .9
+	thetaEst = np.random.rand(N)*.00001;  # t0, t1, .... , tn+1
 	thetas, lossArr = gradDescent.gradientDescentLoop(hypFunc, lossFunc, X, y, thetaEst, alpha, nIter)
 
-	#gradDescent.plotCost(thetas, lossArr, True)
+	gradDescent.plotCost(thetas, lossArr, False)
+
 	#gradDescent.plotCost3D(thetas, lossArr,X,y, True)
 	thetaFinal = thetas[-1];
 	print i, '\n ThetaRef', thetaRef, '\n ThetaEst', thetas[-1], '\n Error', thetaFinal - thetaRef
 
 	x = xRange[1] ;
-	
-	
-	plt.scatter(X[:,1],X[:,2],c=y)
-	m = -thetaFinal[1]/thetaFinal[2]
-	c = -thetaFinal[0]/thetaFinal[2]
-	print "m", m
-	print "c", c
-	f = (m*x+c);
+	print thetaRef / np.max(np.abs(thetaRef))
+	print thetaFinal / np.max(np.abs(thetaFinal))
+	n = N -1;
+	if(n==3):  #3 feature case.
+		gradDescent.plotDataAndDecisionBoundary3D(X, thetaFinal, y, True)
 
-	plt.plot(x,f,'b+-')
-	plt.show()
-
+i = 0
 main()
 
--- Ahmad/utils/gradient/gradientDescent.py
+++ Ahmad/utils/gradient/gradientDescent.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
 def gradientDescent(hypFunc, lossFunc, i, X, y, alpha, theta):
 	m = X.shape[0];
@@ -9,7 +10,10 @@ def gradientDescent(hypFunc, lossFunc, i, X, y, alpha, theta):
 	hy = hypFunc(theta, X) - y;
 	p = hy * X.T;
 	dJdx =  np.sum(p,1);
-	theta = theta - (alpha / m) * dJdx
+	theta = theta - (alpha *1.0 / m) * dJdx
+	#if(np.max(np.abs(theta))>):
+	#theta = theta / sum(theta)
+	#theta = theta / theta[2]
 	loss = lossFunc(theta, X, y)
 	return theta, loss
 
@@ -22,15 +26,7 @@ def gradientDescentLoop(hypFunc, lossFunc, X, y, thetaEst, alpha, nIterations):
 		thetas.append(thetaEst);
 	return thetas, lossArr
 
-def plotCost(thetas, cost, block):
-	plt.subplot(211)
-	plt.plot(cost)
-	plt.ylabel('Loss');
-	
-	plt.legend(['Loss Function']);
-	plt.grid()
-
-	plt.subplot(212)
+def plotThetas(thetas, ylabel):
 	plt.plot(thetas)
 	plt.grid()
 	nt = thetas[0].shape[0]
@@ -39,8 +35,28 @@ def plotCost(thetas, cost, block):
 	for t in range(nt):
 		lgnd.append("theta" + str(t));
 	plt.legend(lgnd);
-	plt.ylabel('Theta');
+	plt.ylabel(ylabel);
 	plt.xlabel('Iterations');
+
+def plotCost(thetas1, cost, block):
+	thetas = np.copy(thetas1)	
+	print " thetas", thetas.shape
+	#	maxVal = np.max(thetas[-1]);
+	for i in range(thetas.shape[0]):
+		thetas[i,:] = thetas[i,:] / thetas[i,0]
+	#thetas = thetas / thetas[0]
+	#print thetas
+	plt.subplot(311)
+	plt.plot(cost)
+	plt.ylabel('Loss');
+	
+	plt.legend(['Loss Function']);
+	plt.grid()
+
+	plt.subplot(312)
+	plotThetas(thetas1, 'theta')
+	plt.subplot(313)
+	plotThetas(thetas, 'normalized theta')
 	plt.show(block = block)
 
 
@@ -68,9 +84,36 @@ def plotCost3D(thetas, cost, X, y, block):
 
 	ax3 = fig.add_subplot(gs[6:,0], projection='3d')
 	ax3.scatter(X[:, 1], X[:, 2], y, c=y, marker='o')
-	ax3.set_xlabel('X1 Feature X')
-	ax3.set_ylabel('X2 Feature Y')
-	ax3.set_zlabel('Y  labels Z')
+	ax3.set_xlabel('X1')
+	ax3.set_ylabel('X2')
+	ax3.set_zlabel('Y')
 	# plt.plot(x1, x2, h)
 	plt.show(block=block)
 
+def plotDataAndDecisionBoundary3D(X, thetaFinal, y, block):
+	x1 = X[:,1];
+	x2 = X[:,2];
+	x3 = X[:,3];
+	
+	fig = plt.figure()
+	ax = fig.add_subplot(111,projection='3d')
+	ax.scatter(x1,x2,x3,c=y,marker='o')
+	ax.set_xlabel('x1')
+	ax.set_ylabel('x2')
+	ax.set_zlabel('x3')
+
+	x1min = np.min(x1);
+	x1max = np.max(x1);
+	x2min = np.min(x2);
+	x2max = np.max(x2);
+	x1 = np.array([x1min,1,1,x1min])
+	x2 = np.array([x2min,x2min, x2max, x2max])
+	#x3 = np.array([.5, .5, .5, .5])
+
+	t0 = thetaFinal[0]/thetaFinal[3]
+	t1 = thetaFinal[1]/thetaFinal[3]
+	t2 = thetaFinal[2]/thetaFinal[3]
+	x3 = -t0 - t1*x1 - t2*x2; 
+	verts = [zip(x1, x2, x3)]
+	ax.add_collection3d(Poly3DCollection(verts))
+	plt.show(block = block)
