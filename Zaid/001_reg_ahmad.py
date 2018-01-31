f7a556fdf471c4a8f761075afd75d1a437ec7d4c
Ahmad Hassan <ahmadhassanch@hotmail.com>
2018-01-31 21:24:14 +0500

    cleanup

--- Zaid/001_reg_ahmad.py
+++ Zaid/001_reg_ahmad.py
@@ -25,12 +25,12 @@ suggested_m_shape = [1,input_length]
 print weights.shape
 print suggested_m_shape
 #exit()
-tensor_m = tf.truncated_normal(suggested_m_shape, 1.0/input_length)
+tWeights = tf.zeros(suggested_m_shape);
 suggested_c = tf.Variable(tf.truncated_normal(shape = [1,], stddev = 1.0/input_length))
 
-suggested_m = tf.Variable(tensor_m)
+vWeights = tf.Variable(tWeights)
 
-y_pred = tf.matmul(suggested_m,x_holder) + suggested_c
+y_pred = tf.matmul(vWeights,x_holder) + suggested_c
 
 loss = lossFunc(y_holder,y_pred)
 train_op = tf.train.AdamOptimizer(10).minimize(loss)
@@ -43,17 +43,17 @@ with tf.Session() as sess:
 	sess.run(init)
 
 	for i in xrange(100000):
-			yret,lret,_,_suggested_m, _suggested_c =  sess.run([y_pred,loss,train_op,suggested_m, suggested_c], feeddict)
+			yret,lret,_,_vWeights, _suggested_c =  sess.run([y_pred,loss,train_op,vWeights, suggested_c], feeddict)
 			
-			print i,lret,_suggested_m, _suggested_c
+			print i,lret,_vWeights, _suggested_c
 			if lret <.0000001 : 
-				print "Converged", _suggested_m, _suggested_c
+				print "Converged", _vWeights, _suggested_c
 				break;
 
 	print np.asarray(yret.shape)
 	print np.asarray(lret)
 
-print "Converged Loss = ", lret, ", m = ", _suggested_m,", c = ", _suggested_c
+print "Converged Loss = ", lret, ", m = ", _vWeights,", c = ", _suggested_c
 
 
 
