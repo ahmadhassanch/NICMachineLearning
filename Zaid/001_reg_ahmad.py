e5cca45acab020cdeeb5cb07b2950bf737f985c6
Ahmad Hassan <ahmadhassanch@hotmail.com>
2018-01-31 21:37:25 +0500

    clean implementation of TensorFlow

--- Zaid/001_reg_ahmad.py
+++ Zaid/001_reg_ahmad.py
@@ -18,42 +18,41 @@ c = bias; #np.asarray([bias])
 y = np.matmul(weights,x) + c;			# array of ground truths
 
 # using x and y figure out a and b
-x_holder = tf.placeholder(tf.float32, x.shape)
-y_holder = tf.placeholder(tf.float32, y.shape)
+hX = tf.placeholder(tf.float32, x.shape)
+hY = tf.placeholder(tf.float32, y.shape)
 
 suggested_m_shape = [1,input_length]
-print weights.shape
-print suggested_m_shape
-#exit()
+
 tWeights = tf.zeros(suggested_m_shape);
-suggested_c = tf.Variable(tf.zeros([1])
+tBias = tf.zeros([1]);
 
-vWeights = tf.Variable(tWeights)
+vB = tf.Variable(tBias)
+vW = tf.Variable(tWeights)
 
-y_pred = tf.matmul(vWeights,x_holder) + suggested_c
+vYref = tf.matmul(vW,hX) + vB
 
-loss = lossFunc(y_holder,y_pred)
-train_op = tf.train.AdamOptimizer(10).minimize(loss)
+loss = lossFunc(hY,vYref)
+train_op = tf.train.AdamOptimizer(.1).minimize(loss)
 
 init = tf.global_variables_initializer()
-feeddict = {x_holder: x, y_holder:y}
+feeddict = {hX: x, hY:y}
 
 with tf.Session() as sess:
 	
 	sess.run(init)
 
 	for i in xrange(100000):
-			yret,lret,_,_vWeights, _suggested_c =  sess.run([y_pred,loss,train_op,vWeights, suggested_c], feeddict)
+			yret,lret,_,_vW, _vB =  sess.run([vYref,loss,train_op, vW, vB], feeddict)
 			
-			print i,lret,_vWeights, _suggested_c
-			if lret <.0000001 : 
-				print "Converged", _vWeights, _suggested_c
+			print i,lret,_vW, _vB
+			if lret <.00001 : 
+				print "Converged", _vW, _vB
 				break;
 
 	print np.asarray(yret.shape)
 	print np.asarray(lret)
 
-print "Converged Loss = ", lret, ", m = ", _vWeights,", c = ", _suggested_c
+print "Converged Loss = ", lret, ", m = ", _vW,", c = ", _vB
 
 
 
