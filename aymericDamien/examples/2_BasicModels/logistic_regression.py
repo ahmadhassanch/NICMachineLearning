80fecbf314193c6cccb6ff2ed63bcae76d89c605
Ahmad Hassan <ahmadhassanch@hotmail.com>
2018-02-08 18:22:55 +0500

    working on logstic reg

--- aymericDamien/examples/2_BasicModels/logistic_regression.py
+++ aymericDamien/examples/2_BasicModels/logistic_regression.py
@@ -1,51 +1,34 @@
-'''
-A logistic regression learning algorithm example using TensorFlow library.
-This example is using the MNIST database of handwritten digits
-(http://yann.lecun.com/exdb/mnist/)
-
-Author: Aymeric Damien
-Project: https://github.com/aymericdamien/TensorFlow-Examples/
-'''
-
 from __future__ import print_function
-
 import tensorflow as tf
 
 # Import MNIST data
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
 
-# Parameters
 learning_rate = 0.01
 training_epochs = 25
 batch_size = 100
 display_step = 1
 
-# tf Graph Input
 x = tf.placeholder(tf.float32, [None, 784]) # mnist data image of shape 28*28=784
 y = tf.placeholder(tf.float32, [None, 10]) # 0-9 digits recognition => 10 classes
 
-# Set model weights
+# Set model weights and Model
 W = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
-
-# Construct model
-pred = tf.nn.softmax(tf.matmul(x, W) + b) # Softmax
+pred = tf.nn.softmax(tf.matmul(x, W) + b) 
 
 # Minimize error using cross entropy
 cost = tf.reduce_mean(-tf.reduce_sum(y*tf.log(pred), reduction_indices=1))
 # Gradient Descent
 optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
 
-# Initialize the variables (i.e. assign their default value)
+sess = tf.Session();
 init = tf.global_variables_initializer()
 
-# Start training
+
 with tf.Session() as sess:
-
-    # Run the initializer
     sess.run(init)
-
     # Training cycle
     for epoch in range(training_epochs):
         avg_cost = 0.
