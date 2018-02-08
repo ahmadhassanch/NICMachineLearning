dbbe244cd47de060134101c621946eb87b6a4a52
Ahmad Hassan <ahmadhassanch@hotmail.com>
2018-02-08 17:55:55 +0500

    linReg working

--- aymericDamien/README.md
+++ aymericDamien/README.md
@@ -1,3 +1,5 @@
+https://github.com/aymericdamien/TensorFlow-Examples
+
 # TensorFlow Examples
 
 This tutorial was designed for easily diving into TensorFlow, through examples. For readability, it includes both notebooks and source codes with explanation.
--- aymericDamien/examples/2_BasicModels/linear_regression.py
+++ aymericDamien/examples/2_BasicModels/linear_regression.py
@@ -1,27 +1,21 @@
-'''
-A linear regression learning algorithm example using TensorFlow library.
-
-Author: Aymeric Damien
-Project: https://github.com/aymericdamien/TensorFlow-Examples/
-'''
-
 from __future__ import print_function
 
 import tensorflow as tf
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
-rng = numpy.random
 
 # Parameters
-learning_rate = 0.01
-training_epochs = 1000
+learning_rate = 0.0000001
+training_epochs = 100
 display_step = 50
 
-# Training Data
-train_X = numpy.asarray([3.3,4.4,5.5,6.71,6.93,4.168,9.779,6.182,7.59,2.167,
-                         7.042,10.791,5.313,7.997,5.654,9.27,3.1])
-train_Y = numpy.asarray([1.7,2.76,2.09,3.19,1.694,1.573,3.366,2.596,2.53,1.221,
-                         2.827,3.465,1.65,2.904,2.42,2.94,1.3])
+data_X = np.asarray([ 1361.7, 1815,  1788.2,  803.31,   968.6,  1646.8,  1861.6,  1760.1,  1099.4,  1252.1,  1864.6,  499.01,  873.85,  666.93,  1694.9,  326.32,  705.85,  1199.1,  1314.1,  937.62,  665.17,  950.14,   887.7,   373.4,  436.31,  1297.8,  1381.1,  1713.8,  1099.7,  752.96,  505.58,  287.78,  606.47,  860.55,  1222.1,  1225.5,  1601.8,  1262.4,  399.19,  1353.2,  329.63,  858.52,  717.15,  1534.6,  320.58,  1562.9,  633.28,  615.64,  221.58,  438.45,   576.9,  585.53,  1830.4,  699.99,  374.78,  1609.1,  1401.5,  1302.8,  818.83,  1442.6,  620.41,  754.45,  843.14,  590.06,  1419.1,  1524.8,  218.03,  840.65,  631.92,  1869.7,  1573.4,   882.9,  1434.7,    1458,  1262.7,  1931.7,  1606.4,  1169.2,  369.45,  1794.8,  663.49,  1034.3,  795.44,  1562.2,  1645.1,  1645.6,  1777.9,  357.45,  1402.2,  1104.4,  804.01,  1259.4,  833.48,  572.09,  671.88,  377.57,  470.11,  777.83,  1269.9,  476.73])
+data_Y = np.asarray([31.857,42.914 ,46.076 ,20.993 ,22.318 ,42.266 ,44.849 ,42.103 , 28.39 ,29.526 ,44.403 , 10.65 , 19.42 ,15.119 ,40.226 ,5.8472 ,16.686 ,30.056 ,32.321 ,25.916 ,16.327 ,22.011 ,23.583 ,10.541 , 8.846 ,32.298 ,33.493 ,42.379 ,27.143 , 20.68 ,13.676 ,4.7659 ,12.785 ,20.199 ,31.044 ,32.768 ,39.217 ,31.276 ,7.9755 ,34.906 ,9.3491 ,22.875 ,20.139 ,36.103 ,9.5162 ,38.906 ,14.728 ,13.115 ,6.9772 ,12.957 ,15.801 ,16.325 ,44.983 ,15.956 ,8.7618 ,39.009 ,32.646 ,31.537 ,19.173 ,34.578 ,14.751 ,19.047 ,  19.2 ,13.215 ,35.421 ,37.656 ,6.6254 ,20.016 ,17.334 ,48.811 , 40.66 ,21.243 ,36.965 ,34.117 ,32.146 ,46.353 ,37.914 ,27.005 ,6.8673 ,47.148 ,16.713 ,27.139 ,20.335 ,40.249 ,40.225 ,42.881 ,44.417 ,7.0105 ,36.617 ,28.443 ,19.509 ,32.579 , 20.43 ,12.082 ,17.222 ,8.4334 ,11.301 ,19.876 ,30.826 ,10.155])
+train_X = data_X[0:70]
+train_Y = data_Y[0:70]
+test_X = data_X[70:]
+test_Y = data_Y[70:]
+
 n_samples = train_X.shape[0]
 
 # tf Graph Input
@@ -29,61 +23,43 @@ X = tf.placeholder("float")
 Y = tf.placeholder("float")
 
 # Set model weights
-W = tf.Variable(rng.randn(), name="weight")
-b = tf.Variable(rng.randn(), name="bias")
+W = tf.Variable(np.random.randn(), name="weight")
+b = tf.Variable(np.random.randn(), name="bias")
 
 # Construct a linear model
 pred = tf.add(tf.multiply(X, W), b)
 
-# Mean squared error
-cost = tf.reduce_sum(tf.pow(pred-Y, 2))/(2*n_samples)
-# Gradient descent
-#  Note, minimize() knows to modify W and b because Variable objects are trainable=True by default
+cost = tf.reduce_mean(tf.square(pred-Y))
 optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
 
-# Initialize the variables (i.e. assign their default value)
 init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
 
-# Start training
-with tf.Session() as sess:
 
-    # Run the initializer
-    sess.run(init)
+for epoch in range(training_epochs):
+    sess.run(optimizer, feed_dict={X: train_X, Y: train_Y})
 
-    # Fit all training data
-    for epoch in range(training_epochs):
-        for (x, y) in zip(train_X, train_Y):
-            sess.run(optimizer, feed_dict={X: x, Y: y})
+    # Display logs per epoch step
+if (epoch+1) % display_step == 0:
+    c = sess.run(cost, feed_dict={X: train_X, Y:train_Y})
+    print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(c), \
+        "W=", sess.run(W), "b=", sess.run(b))
 
-        # Display logs per epoch step
-        if (epoch+1) % display_step == 0:
-            c = sess.run(cost, feed_dict={X: train_X, Y:train_Y})
-            print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(c), \
-                "W=", sess.run(W), "b=", sess.run(b))
+print("Optimization Finished!")
+training_cost = sess.run(cost, feed_dict={X: train_X, Y: train_Y})
+print("Training cost=", training_cost, "W=", sess.run(W), "b=", sess.run(b), '\n')
 
-    print("Optimization Finished!")
-    training_cost = sess.run(cost, feed_dict={X: train_X, Y: train_Y})
-    print("Training cost=", training_cost, "W=", sess.run(W), "b=", sess.run(b), '\n')
+# Graphic display
+plt.plot(train_X, train_Y, 'ro', label='Original data')
+plt.plot(train_X, sess.run(W) * train_X + sess.run(b), label='Fitted line')
+plt.legend()
+plt.show()
 
-    # Graphic display
-    plt.plot(train_X, train_Y, 'ro', label='Original data')
-    plt.plot(train_X, sess.run(W) * train_X + sess.run(b), label='Fitted line')
-    plt.legend()
-    plt.show()
+testing_cost = sess.run(cost, feed_dict={X: test_X, Y: test_Y})  # same function as cost above
+print("Testing cost=", testing_cost)
 
-    # Testing example, as requested (Issue #2)
-    test_X = numpy.asarray([6.83, 4.668, 8.9, 7.91, 5.7, 8.7, 3.1, 2.1])
-    test_Y = numpy.asarray([1.84, 2.273, 3.2, 2.831, 2.92, 3.24, 1.35, 1.03])
-
-    print("Testing... (Mean square loss Comparison)")
-    testing_cost = sess.run(
-        tf.reduce_sum(tf.pow(pred - Y, 2)) / (2 * test_X.shape[0]),
-        feed_dict={X: test_X, Y: test_Y})  # same function as cost above
-    print("Testing cost=", testing_cost)
-    print("Absolute mean square loss difference:", abs(
-        training_cost - testing_cost))
-
-    plt.plot(test_X, test_Y, 'bo', label='Testing data')
-    plt.plot(train_X, sess.run(W) * train_X + sess.run(b), label='Fitted line')
-    plt.legend()
-    plt.show()
+plt.plot(test_X, test_Y, 'bo', label='Testing data')
+plt.plot(train_X, sess.run(W) * train_X + sess.run(b), label='Fitted line')
+plt.legend()
+plt.show()
