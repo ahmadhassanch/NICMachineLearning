bd3e4438bc90309e0eeabbcdc5ace2f20ad1fbf1
Ahmad Hassan <ahmadhassanch@hotmail.com>
2018-02-11 06:51:49 +0500

    Added TFLearn

--- Faisal/trainOneVsAll.py
+++ Faisal/trainOneVsAll.py
@@ -2,6 +2,8 @@ import numpy as np
 import pdb
 import scipy.optimize as op
 from hypothesis import hypothesis
+import lrCostFunction as lrcf
+
 
 def lrCostFunction(theta, X, y, Lambda):
 	m = len(y);
@@ -29,12 +31,15 @@ def trainOneVsAll(X, y, num_labels, Lambda):
 		print "Training", c ,"category...", num_labels
 		Y = (y==c).astype(int)   # converted True/False to 0/1
 		initial_theta = np.zeros((n, 1))
-
+		
+		theta = op.minimize(lrCostFunction, x0=initial_theta, args=(X, Y, Lambda), options={'disp': True, 'maxiter':13}, method="Newton-CG", jac=True)
+		'''
 		theta = op.minimize(lrCostFunction, x0=initial_theta,
 											args=(X, Y, Lambda),
 											options={'disp': True, 'maxiter':13},
 											method="Newton-CG",
 											jac=True)
+		'''
 		all_theta[c,:] = theta["x"]
 
 	return all_theta
